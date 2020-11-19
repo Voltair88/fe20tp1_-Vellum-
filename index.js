73f368a729d9eacc311b9df3a3384deb4fbf3188
Emma <70127722+Nonis17@users.noreply.github.com>
2020-11-19 11:44:03 +0100

    Added JS to X btn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+//När sidan laddas kollar vi om besökaren besökt sidan tidigare
 document.addEventListener("DOMContentLoaded", function() {
     pageOnLoad();
   });
 
-
 let popUp = document.querySelector('.button-bg'); 
-
 
 function pageOnLoad(){
     if (localStorage.length === 0){
@@ -18,4 +17,10 @@ function pageOnLoad(){
     }
 }
 
+//När man klickar på X knappen i popupen försvinner rutan
+var xButton = document.querySelector('.x-close'); 
+
+xButton.addEventListener('click', function() {
+    popUp.classList.remove('bg-active');
+});
 
