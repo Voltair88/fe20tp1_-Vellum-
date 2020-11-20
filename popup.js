4d726ab2fbe8a8704e668a8d7059f164b6af1c20
Nonis17 <emma.lilja@student.kyh.se>
2020-11-20 07:32:10 +0100

    Popup merged with text editor

---
+++ popup.js
@@ -0,0 +1,26 @@
+//När sidan laddas kollar vi om besökaren besökt sidan tidigare
+document.addEventListener("DOMContentLoaded", function() {
+    pageOnLoad();
+  });
+
+let popUp = document.querySelector('.button-bg'); 
+
+function pageOnLoad(){
+    if (localStorage.length === 0){
+    
+        popUp.classList.add('bg-active');
+        localStorage.setItem('key',"varit");
+    }
+    else {
+        popUp.classList.add('button-bg');
+
+    }
+}
+
+//När man klickar på X knappen i popupen försvinner rutan
+var xButton = document.querySelector('.x-close'); 
+
+xButton.addEventListener('click', function() {
+    popUp.classList.remove('bg-active');
+});
+
