d93e0e306dfdb4ac0b3a33ef6d112f64b2ef5ffb
filipclavin <filip.clavin@student.kyh.se>
2020-11-25 15:00:04 +0100

    merged devFilip and main

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ tinymce.init({
     toolbar: 'SaveButton|undo redo | styleselect fontselect fontsizeselect | bold italic|PrintDoc | DeleteButton | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent',
 
     height: 800,
-    
-//////////////////Added custom save button and Print btn
-    setup: function(mytextarea) {
-    
+
+    //////////////////Added custom save button and Print btn
+    setup: function (mytextarea) {
+
         mytextarea.ui.registry.addButton('SaveButton', {
             icon: 'save',
             text: 'Save',
             onAction: function () {
                 saveNote(edit);
             }
-          });
+        });
 
         mytextarea.ui.registry.addButton('PrintDoc', {
             icon: 'print',
             onAction: function () {
-              alert('Wait..Print function is not implemented yet!');
+                alert('Wait..Print function is not implemented yet!');
             }
-          });
+        });
 
         //Clear local storage
         mytextarea.ui.registry.addButton('DeleteButton', {
@@ -31,14 +31,14 @@ tinymce.init({
                 clearLocalStorage();
             }
         });
-        
+
     }
-    
-    
-    
+
+
+
     ////////////////////
-    
-    
+
+
 });
 
 let mytextArea = document.getElementById("mytextarea");
@@ -50,67 +50,80 @@ let edit = false; // to check whether it is an edit or new note when saving
 let clickedDiv;
 let favToggle = document.getElementById("favToggle");
 
+//To update note
+let globalTextContent;
+let globalSubject;
 
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
     pageOnLoadFunction();
-  });
-  
+});
 
-function fetchLocalStorageLastKey(){
+
+
+
+function fetchLocalStorageLastKey() {
     //check browser support
-    if(typeof(Storage) !== "undefined"){
+    if (typeof (Storage) !== "undefined") {
         currentKey = localStorage.length;
         return true;
     }
 
 }
 
-function saveNote(edit){
+function saveNote(edit) {
+
 
     let today = new Date();
     let myContent = tinymce.get("mytextarea").getContent();
     let obj = {};
-    if(myContent != '' && subjectEl.value != ''){
-        if(!edit){   //This block for new entries
-            if(fetchLocalStorageLastKey()){
+
+    if (myContent != '' && subjectEl.value != '') {
+        if (!edit) {   //This block for new entries
+            if (fetchLocalStorageLastKey()) {
+
                 obj['id'] = localStorage.length;
                 obj['note'] = myContent;
-                obj['date'] = today;
+                obj['date'] = today.toLocaleDateString();
                 obj['favorite'] = false;
                 obj['subject'] = subjectEl.value;
-    
+
                 localStorage.setItem(currentKey, JSON.stringify(obj));
                 displaySavedNoteElement(obj);  //Display saved note in left panel
                 tinymce.activeEditor.windowManager.alert('Successfully saved');
                 tinymce.get("mytextarea").setContent("");
                 subjectEl.value = "";
-                
-            }else{ //This block is for edit 
-                
-            }
-        }else{
 
-            if(myContent.localeCompare(JSON.parse(localStorage.getItem(clickedDiv.id)).note) === 0){
+            } else { 
+                tinymce.activeEditor.windowManager.alert('Object not found.!');
+            }
+        } else { //This block is for edit 
+
+            if (myContent.localeCompare(JSON.parse(localStorage.getItem(clickedDiv.id)).note) === 0
+                && subjectEl.value.localeCompare(JSON.parse(localStorage.getItem(clickedDiv.id)).subject) === 0 ) {
                 tinymce.get("mytextarea").setContent("");
                 subjectEl.value = "";
                 tinymce.activeEditor.windowManager.alert('No changes to save');
-                  
-            }else{
-                //Textarea has changed, ask uset to create a new note
 
-                tinymce.activeEditor.windowManager.confirm("Do you want to save changes", function(s) {
-                    if (s)
-                        if(updateRecord()){
+            } else {
+                //Textarea has changed, ask uset to create a new note
+                globalSubject = document.getElementById("subjectTextfieldId").value;
+                globalTextContent = tinyMCE.activeEditor.getContent();     //Set textarea content and subject as global, otherwise below line of code reset those values
+
+                tinymce.activeEditor.windowManager.confirm("Do you want to save changes", function (s) {
+                    if (s){
+                        if (updateRecord()) {
                             tinymce.activeEditor.windowManager.alert('Successfully saved');
                         }
-                        else{
+                        else {
                             tinymce.activeEditor.windowManager.alert('Save error..!');
                         }
-                    else{
+                        }
+                    else {
                         tinymce.get("mytextarea").setContent("");
                         subjectEl.value = "";
                     }
-                 });
+                });
 
                 /* saveNote(false); */
                 tinymce.get("mytextarea").setContent("");
@@ -118,8 +131,8 @@ function saveNote(edit){
             }
 
         }
-        
-    }else{
+
+    } else {
         tinymce.activeEditor.windowManager.alert('Text area or/and subject is empty..! Save error..!');
     }
 
@@ -128,17 +141,16 @@ function saveNote(edit){
 }
 
 
-
 //Read localStorage and display in left panel
-function pageOnLoadFunction(){
+function pageOnLoadFunction() {
 
     let div;
     let p;
     let h1;
 
-    for(let i=1; i < localStorage.length;i++){
-        if(localStorage.key !== 0){
-            
+    for (let i = 1; i < localStorage.length; i++) {
+        if (localStorage.key !== 0) {
+
             div = document.createElement('div');
             div.id = i;
             //div.innerText = JSON.parse(localStorage.getItem(i)).time;
@@ -146,21 +158,26 @@ function pageOnLoadFunction(){
 
             p = document.createElement('p');
             let objNote = JSON.parse(localStorage.getItem(i))
-            p.innerHTML = objNote.subject; 
-
+            p.innerHTML = objNote.subject;
             div.appendChild(p);
             let newStar = createStar();
             div.appendChild(newStar);
-            console.log(div);
-        
-            div.addEventListener("click", function(){onClickDiv(event,this)},true);
+
+            pDate = document.createElement('p');
+            pDate.innerHTML = objNote.date;   //.Date  .toLocaleDateString()
+            pDate.className = 'pDate';
+            div.appendChild(pDate);
+
+            div.addEventListener("click", function () { onClickDiv(event, this) }, true);
 
             leftCanvas.appendChild(div);
-        }   
+        }
+
+
     }
 }
 
-function onClickDiv(event){
+function onClickDiv(event) {
 
     edit = true;
 
@@ -175,46 +192,55 @@ function onClickDiv(event){
     subjectEl.value = objNote.subject;
 }
 
-function clearLocalStorage(){
-    localStorage.clear();
+function clearLocalStorage() {
+    tinymce.activeEditor.windowManager.confirm("Do you want to delete all", function (s) {
+        if (s){
+            tinymce.activeEditor.windowManager.alert('Deleted all');
+            localStorage.clear();
+            location.reload();
+            }
+    });
 }
 
 //Display saved note element direct after saving wihtout loading it from the localStorage
-function displaySavedNoteElement(obj){
-    console.log(star);
+function displaySavedNoteElement(obj) {
     div = document.createElement('div');
     div.className = 'divTag';
-    div.id = localStorage.length-1;  // Key 0 is to "check user har varit" , to remove that id use -1
+    div.id = localStorage.length - 1;  // Key 0 is to "check user har varit" , to remove that id use -1
     p = document.createElement('p');
     p.innerHTML = obj.subject;   //.subject;
 
     div.appendChild(p);
     let newStar = createStar();
     div.appendChild(newStar);
-    div.addEventListener("click", function(){onClickDiv(event,this)},true);
+
+    pDate = document.createElement('p');
+    pDate.innerHTML = obj.date;   //.Date
+    pDate.className = 'pDate';
+    div.appendChild(pDate);
+    
+    div.addEventListener("click", function () { onClickDiv(event, this) }, true);
 
     leftCanvas.appendChild(div);
-} 
+}
 
-function updateRecord(){
-    try{
-        let obj = JSON.parse(localStorage.getItem(clickedDiv.id));
+function updateRecord() {
+
+    let today = new Date();
+    let obj = JSON.parse(localStorage.getItem(clickedDiv.id));
+
+    obj['id'] = clickedDiv.id;
+    obj['note'] = globalTextContent;
+    obj['date'] = today.toLocaleDateString();
+    obj['favorite'] = false;
+    obj['subject'] = globalSubject;
+
+    //Update the subject div in left panel
+    clickedDiv.firstChild.innerText = globalSubject;
+    /* var user = JSON.parse(localStorage.getItem('user')); */
+    localStorage.setItem(clickedDiv.id, JSON.stringify(obj));
     
-        obj['id'] = clickedDiv.id;
-        obj['note'] = myContent;
-        obj['date'] = today;
-        obj['favorite'] = false;
-        obj['subject'] = subjectEl.value;
-
-
-        localStorage.setItem(JSON.stringify(clickedDiv.id), JSON.stringify(obj));
-
-        return true;
-    }catch(err){
-        tinymce.activeEditor.windowManager.alert('Update error..!');
-    }
-    
-    edit = false;
+    return true;
 }
 
 //Funktion som skapar en stjärna till en anteckning
@@ -223,42 +249,32 @@ function createStar() {
     newStar.setAttribute('class', 'star');
     newStar.setAttribute('src', './images/star.png');
     newStar.setAttribute('alt', 'favorite button unlit');
+    
+    let favObj;
 
-    newStar .addEventListener('click', function(e){
-        e.target.parentElement.classList.add('favorite')
-        e.target.parentElement.appendChild(createFavStar());
+    newStar.addEventListener('click', function(e){
+        let targetID = e.target.parentElement.getAttribute('id')
+        favObj = JSON.parse(localStorage.getItem(targetID));
+
+        if(e.target.parentElement.classList.contains("favorite")){
+            newStar.setAttribute('src', './images/star.png');
+            newStar.setAttribute('alt', 'favorite button unlit');
+            e.target.parentElement.classList.remove('favorite');
+            
+            favObj.favorite = false;
+        } else {
+            newStar.setAttribute('src', './images/favstar.png');
+            newStar.setAttribute('alt', 'favorite button lit');
+            e.target.parentElement.classList.add('favorite');
+
+            favObj.favorite = true;
+        }
         
-        let favObj = JSON.parse(localStorage.getItem(e.target.parentElement.getAttribute('id')));
-        favObj.favorite = true;
-        
-        localStorage.setItem(e.target.parentElement.getAttribute('id'), JSON.stringify(favObj));
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[1]);
+    localStorage.setItem(e.target.parentElement.getAttribute('id'), JSON.stringify(favObj));
     
     })
 
     return newStar;
-}
-
-//Funktion som skapar en favoritstjärna till en anteckning
-function createFavStar() {
-    let favStar = document.createElement('img');
-    favStar.setAttribute('class', 'favstar');
-    favStar.setAttribute('src', './images/favstar.png');
-    favStar.setAttribute('alt', 'favorite button lit');
-
-    favStar.addEventListener('click', function(e){
-        e.target.parentElement.classList.remove('favorite')
-        e.target.parentElement.appendChild(createStar());
-    
-        let unFavObj = JSON.parse(localStorage.getItem(e.target.parentElement.getAttribute('id')));
-        unFavObj.favorite = false;
-        
-        localStorage.setItem(e.target.parentElement.getAttribute('id'), JSON.stringify(unFavObj));
-        e.target.parentElement.removeChild(e.target.parentElement.childNodes[1]);
-    
-    })
-
-    return favStar;
 }
 
 favToggle.addEventListener('change', function(e){
@@ -278,4 +294,3 @@ favToggle.addEventListener('change', function(e){
     }
 })
     
-/* var user = JSON.parse(localStorage.getItem('user')); */
